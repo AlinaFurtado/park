ec3489c260dc31a03125552311f2d56be65a3d1b
AlinaFurtado <alinaannie@gmail.com>
2020-05-13 16:29:46 +0530

    Updates

---
+++ precache-manifest.b27d0c8f0e6b02c8ec29ffa91678db65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a510dc527fcfe3f5a3b2db74fa4b78",
+    "url": "/park/index.html"
+  },
+  {
+    "revision": "c4fd49116fdd2705511e",
+    "url": "/park/static/css/main.f6439445.chunk.css"
+  },
+  {
+    "revision": "3319e25d7f43098e1745",
+    "url": "/park/static/js/2.269e25e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/park/static/js/2.269e25e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4fd49116fdd2705511e",
+    "url": "/park/static/js/main.12ece0d2.chunk.js"
+  },
+  {
+    "revision": "7f6711d4111f539b8427",
+    "url": "/park/static/js/runtime-main.c2c95cb9.js"
+  }
+]);
